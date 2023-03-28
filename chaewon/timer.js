61d8c469aa0d91a3ee0a3da00c1bb73fd2398a33
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-03-28 17:32:21 +0900

    timer_css 변경1

--- chaewon/timer.js
+++ chaewon/timer.js
@@ -32,51 +32,62 @@ $ddaybtn.onclick = () => {
     $restDay.innerText = `D - ${until} `
 }
 
+const $settingIcon = document.getElementById('timsettingIcon');
+console.log($settingIcon);
+$settingIcon.onclick = function (){
+    $dday.classList.toggle('displayBlock');
+   $ddaybtn.classList.toggle('displayBlock');
+}
+
 
 //time-out구현...모르겠다..
 //25분
-let sec = 59;
-let min = "";
-let time;
 
-const $tim25min = document.querySelector('.tim25min');
+const tim5min = document.querySelector('.tim5min');
+const tim25min = document.querySelector('.tim25min');
+const tim1min = document.querySelector('.tim1min');
 const $restTime = document.querySelector('.restTime');
-// $tim25min.onclick = 
+let sec = 59;
+let min = 0;
+let tim;
 
-function sec() {
-    min= 25;
+//console.log(tim25min);
+
+function timeout() {
     sec--;
-    if(sec < 1) {
-        sec = 0;
-        min--;        
-    }
-}
-
-function sub() {
-    sec();
-    $restTime =  (min > 9 ? min : "0" + min) 
-                 +":"+ (sec > 9 ? sec : "0" + sec);
+    if(sec < 0) {
+        min--;
+        sec = 59; 
+        if(min < 0 ){
+            min = 0;
+            sec= 0; 
+            return;
+        }
+    } 
+    $restTime.textContent = `${min} : ${sec}`; 
     time();
-
 }
+
 function time() {
-    time = setTimeout(sub, 1000);
+    tim = setTimeout(timeout, 1000);
 }
 
-$tim25min.onclick = time;
-
+tim25min.onclick = function() {
+    min= 24;
+    time();
+}   
 
 //5분
+tim5min.onclick= function() {
+    min = 4;
+    time();
 
-
-
-
-
+} 
 
 //1분
-
-
-
+tim1min.onclick = function() {
+    time();
+}
 
 
 
