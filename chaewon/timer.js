d41a5d673dbe62bf428e914f35b2dab864731d16
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-03-28 14:21:58 +0900

    timer1,3 js 완료

--- chaewon/timer.js
+++ chaewon/timer.js
@@ -21,75 +21,49 @@ setInterval(getClock, 1000);
 //dday 설정
 const today = new Date();
 const $dday = document.getElementById('endday');
-let dDay = new Date("2023-12-30");
-const untilDday = new Date(dDay - today);
-const untilDay = String(Math.floor(untilDday/(1000 * 60* 60 * 24)));
+const $ddaybtn = document.getElementById('ddaybtn');
 
-//console.log(untilDay);
+$ddaybtn.onclick = () => {
+    let dDay = new Date($dday.value);
+    //console.log(dDay);
+    const untilDday = new Date(dDay - today);
+    const until = String(Math.floor(untilDday/(1000 * 60* 60 * 24))+1);
+    const $restDay = document.getElementById('d-day');
+    $restDay.innerText = `D - ${until} `
+}
 
-const $restDay = document.getElementById('d-day');
-$restDay.innerText = `D - ${untilDay} `
 
-
-
-
-//time-up구현...모르겠다..
+//time-out구현...모르겠다..
 //25분
+let sec = 59;
+let min = "";
+let time;
 
-const $btn25Min = document.querySelector('.tim25min');
-console.log($btn25Min);
-// $btn25Min.onclick = function restTime() {
-//     const setTime= 1500;
-//     let min = "";
-//     let sec = "";
-//     min = parseInt(setTime/60);
-//     sec = setTime % 60 ; 
-//     document.getElementById('restMin').innerText = `${min}분 ${sec}초`; 
-//     time--; 
-// }
-$btn25Min.addEventListener('click',function restTime() {
-        const setTime= 1500;
-        let min = "";
-        let sec = "";
-        
-       
-        min = parseInt(setTime/60);
-        sec = (setTime%60); 
-        document.getElementById('restMin').innerText =`${min}M ${sec}S`
-        setTime;
-    
-        if(setTime<0) {
-            clearInterval(restTime);
-            document.querySelector('.timRestTime').innerText = "time up!"
-            
-        }
-    } );
+const $tim25min = document.querySelector('.tim25min');
+const $restTime = document.querySelector('.restTime');
+// $tim25min.onclick = 
 
-//setInterval(restTime(), 1000);
+function sec() {
+    min= 25;
+    sec--;
+    if(sec < 1) {
+        sec = 0;
+        min--;        
+    }
+}
 
-// const restTime = setInterval(function() {
-//     const setTime= 1500;
-//     let min = "";
-//     let sec = "";
-    
-    
-//     min = parseInt(time/60);
-//     sec = time % 60 ; 
-//     document.getElementById('restMin').innerHTML = min+"분" +sec +"초"; 
-//     time--;
+function sub() {
+    sec();
+    $restTime =  (min > 9 ? min : "0" + min) 
+                 +":"+ (sec > 9 ? sec : "0" + sec);
+    time();
 
-//     if(time<0) {
-//         clearInterval(restTime);
-//         document.querySelector('.timRestTime').innerText = "time up!"
-        
-//     }
-// }, 1000);
+}
+function time() {
+    time = setTimeout(sub, 1000);
+}
 
-
-
-
-
-
+$tim25min.onclick = time;
 
 
 //5분
@@ -112,6 +86,7 @@ const btnStart = document.querySelector('.LearningTimebtnStart');
 const btnStop = document.querySelector('.LearningTimebtnStop');
 const btnReset = document.querySelector('.LearningTimebtnReset');
 const runTime = document.querySelector('.timRunning');
+const timRecord = document.querySelector('#timRecord');
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
@@ -129,7 +104,7 @@ function second() {
 
         }
     }
-    return;
+    
 }
 
 function add(){
@@ -137,6 +112,8 @@ function add(){
     runTime.textContent = (hours > 9 ? hours : "0" + hours) 
                         +":"+ (minutes > 9 ? minutes : "0" + minutes) 
                         +":"+ (seconds > 9 ? seconds : "0" + seconds);
+    
+    
     timer();
 
 }
@@ -144,11 +121,10 @@ function timer() {
     t = setTimeout(add, 1000);
 }
 
-timer();
-
 btnStart.onclick = timer;
 btnStop.onclick = function() {
     clearTimeout(t);
+    timRecord.textContent = runTime.textContent;
 }
 btnReset.onclick = function() {
     runTime.textContent = "00:00:00";
