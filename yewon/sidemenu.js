1816452d9472485d20d50202d73468865c8b13b2
JOY0987 <jjoyewon@gmail.com>
2023-03-29 23:32:09 +0900

    사이드바 완성

--- yewon/sidemenu.js
+++ yewon/sidemenu.js
@@ -1,15 +1,16 @@
 //버튼의 요소 노드 취득
-const menuBtn = document.querySelector('.gnb-btn');
+// 햄버거 버튼
+const menuBtn = document.querySelector('.gnb-btn'); 
+// 사이드바 < 버튼
 const closeBtn = document.querySelector('#gnb .gnb-close');
-
+// nav 태그
 const gnb = document.querySelector('#gnb');
 
 //클릭 이벤트 생성
 menuBtn.addEventListener('click', () => {
-    alert('버튼클리익');
-    gnb.classList.add('on');
+    gnb.classList.add('on', 'active');
 });
 
 closeBtn.addEventListener('click', () => {
-    gnb.classList.remove('on');
+    gnb.classList.remove('on', 'active');
 });
